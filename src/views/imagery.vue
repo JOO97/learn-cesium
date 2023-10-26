2019e06dc44c7f0da253264dd4f6605ce4c64d1b
JOO97 <18950181492@163.com>
2023-10-26 17:53:47 +0800

    chore(/src): update

---
+++ src/views/imagery.vue
@@ -0,0 +1,83 @@
+<!-- Cesium Imagery  -->
+<template>
+	<!-- 容器 -->
+	<div id="cesiumContainer" style="width: 100vw; height: 100vh" />
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import * as Cesium from 'cesium';
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+import GCJ02TilingScheme from '../core/gcj02TilingScheme';
+
+window.Cesium = Cesium;
+
+let viewer = null;
+
+onMounted(async () => {
+	Cesium.Ion.defaultAccessToken =
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZDk3ZjUxYS0zMzkyLTQ3NWEtYmM1Ny0zMTUxM2ZlMDFjMjMiLCJpZCI6MTczNDg2LCJpYXQiOjE2OTgxMjgzODV9.xSyHyR2dPE4dvRqOh5pl1dFSsTg0zcDyQ0g-82OzqV4';
+
+	viewer = new Cesium.Viewer('cesiumContainer', {
+		terrain: Cesium.Terrain.fromWorldTerrain({
+			requestWaterMask: true,
+		}),
+		// baseLayer: Cesium.ImageryLayer.fromWorldImagery({
+		// 	style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS,
+		// }),
+		baseLayerPicker: false,
+	});
+
+	const layer = viewer.scene.imageryLayers;
+
+	/* 图层叠加 */
+	// const blackMarble = Cesium.ImageryLayer.fromProviderAsync(
+	// 	Cesium.IonImageryProvider.fromAssetId(2327554)
+	// );
+	// blackMarble.alpha = 0.5;
+	// blackMarble.brightness = 2.0;
+	// layer.add(blackMarble);
+
+	// const logoImagery = Cesium.ImageryLayer.fromProviderAsync(
+	// 	Cesium.SingleTileImageryProvider.fromUrl(
+	// 		'../../src/assets/images/Cesium_Logo_Color_Overlay.png',
+	// 		{
+	// 			rectangle: Cesium.Rectangle.fromDegrees(-75.0, 28.0, -67.0, 29.75),
+	// 		}
+	// 	)
+	// );
+	// layer.add(logoImagery);
+
+	/**
+	 * 加载高德瓦片服务
+	 */
+	const aMap = new Cesium.UrlTemplateImageryProvider({
+		url: 'https://webst02.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}',
+		maximumLevel: 18,
+		minimumLevel: 1,
+		credit: 'Amap',
+		// tilingScheme: new GCJ02TilingScheme(), //坐标系转换
+	});
+	layer.addImageryProvider(aMap);
+
+	/* Add Billboard */
+	addBillboard();
+});
+
+/* Add Billboard */
+function addBillboard() {
+	const billboard = viewer.entities.add({
+		position: Cesium.Cartesian3.fromDegrees(118.096697, 24.440254),
+		// billboard: {
+		// 	image: '../../src/assets/images/Cesium_Logo_Color_Overlay.png',
+		// 	width: 100,
+		// 	height: 25,
+		// },
+		point: { pixelSize: 20, color: Cesium.Color.RED },
+	});
+	viewer.flyTo(billboard);
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/terrain.vue
+++ src/views/terrain.vue
@@ -1,4 +1,4 @@
-<!-- Geometry -->
+<!-- TODO: 3D Terrain  https://www.jianshu.com/p/8002fbd3b142  https://blog.csdn.net/qq_40020291/article/details/132201537-->
 <template>
 	<!-- 容器 -->
 	<div id="cesiumContainer" style="width: 100vw; height: 100vh" />
@@ -17,23 +17,13 @@ onMounted(async () => {
 	Cesium.Ion.defaultAccessToken =
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZDk3ZjUxYS0zMzkyLTQ3NWEtYmM1Ny0zMTUxM2ZlMDFjMjMiLCJpZCI6MTczNDg2LCJpYXQiOjE2OTgxMjgzODV9.xSyHyR2dPE4dvRqOh5pl1dFSsTg0zcDyQ0g-82OzqV4';
 
+	/* Enable terrain lighting */
 	viewer = new Cesium.Viewer('cesiumContainer', {
 		terrain: Cesium.Terrain.fromWorldTerrain({
 			requestVertexNormals: true,
 			requestWaterMask: true,
 		}),
 	});
-	// viewer.scene.globe.enableLighting = true;
-
-	viewer.entities.add({
-		rectangle: {
-			coordinates: Cesium.Rectangle.fromDegrees(-100.0, 20.0, -90.0, 30.0),
-			material: new Cesium.StripeMaterialProperty({
-				evenColor: Cesium.Color.WHITESMOKE,
-				oddColor: Cesium.Color.GREENYELLOW,
-				repeat: 5,
-			}),
-		},
-	});
+	viewer.scene.globe.enableLighting = true;
 });
 </script>
